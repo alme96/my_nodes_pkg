6fa6667af3d392ef31ef755a9e702bd11f8582ae
alme96 <alme96@hotmail.it>
2021-02-08 03:15:42 +0100

    sadf

--- include/transform_ekf.h
+++ include/transform_ekf.h
@@ -21,10 +21,14 @@ class Transform_Ekf {
     Transform_Ekf(ros::NodeHandle nh) {
 
       pub  = nh.advertise<nav_msgs::Odometry>("ekf_transf", 100);
-      z_angle = 3.04;
+      z_angle = 3.04; // 8_1
+      // z_angle = -1.570796327; // rectangular_2
+      // z_angle = -1.7; // rectangular_3
       // q_transf = Quaternion(-0.00984011183584, 0.000616954945317, 0.998388208726, 0.055890752745); // 8_1
       q_transf.setRPY(0.0, 0.0, z_angle); // 8_1
       r = {-0.0199131557338, 1.65900934246, 0.900144956133, 1}; // 8_1
+      // r = {-0.944709439143, 1.03761957585, 0.897081033867, 1}; // rectangular_2
+      // r = {-0.954388224872, 0.916989413121, 0.896483918015, 1}; // rectangular_3
       // R_0.getRotation(q_transf);
       // R_0.getRPY(roll, pitch, yaw);
       // tf::Matrix3x3::serializeFloat(R_0);
